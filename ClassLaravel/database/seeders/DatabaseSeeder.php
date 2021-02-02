6181d0d129dd9a92ef84d9b9e18a3c41b2cf8475
Acosta-Jhon <jhonerick2018@gmail.com>
2021-02-02 17:19:55 -0500

    seeders complete

--- ClassLaravel/database/seeders/DatabaseSeeder.php
+++ ClassLaravel/database/seeders/DatabaseSeeder.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Seeder;
 use App\Models\Level;
 use App\Models\User;
 use App\Models\Group;
+use App\Models\Profile;
+use App\Models\Location;
 
 class DatabaseSeeder extends Seeder
 {
@@ -17,7 +19,12 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         Level::factory(5)->create();
-        User::factory(10)->create();
+        User::factory(3)->create();
         Group::factory(5)->create();
+
+        //Relacion 1 - 1 obligado a instanciar en sus propios Seeders
+        $this->call(ProfileSeeder::class);
+        $this->call(LocationSeeder::class);
+
     }
 }
--- ClassLaravel/database/seeders/ProfileSeeder.php
+++ ClassLaravel/database/seeders/ProfileSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace Database\Seeders;
+use App\Models\Profile;
 
 use Illuminate\Database\Seeder;
 
@@ -13,6 +14,28 @@ class ProfileSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $profile = new Profile();
+        $profile->name = 'User1';
+        $profile->description = 'Enjoy';
+        $profile->followers = '25';
+        $profile->following = '50';
+        $profile->user_id = 1;
+        $profile->save();
+
+        $profile2 = new Profile();
+        $profile2->name = 'User2';
+        $profile2->description = 'Enjoy';
+        $profile2->followers = '25';
+        $profile2->following = '50';
+        $profile2->user_id = 2;
+        $profile2->save();
+
+        $profile3 = new Profile();
+        $profile3->name = 'User3';
+        $profile3->description = 'Enjoy';
+        $profile3->followers = '25';
+        $profile3->following = '50';
+        $profile3->user_id = 3;
+        $profile3->save();
     }
 }
